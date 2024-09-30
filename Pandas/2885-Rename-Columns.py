3173885caee47a7e997e95c0796b6d49c51367a3
Eko Mardianto <155344996+MasokPakEko@users.noreply.github.com>
2024-09-30 16:23:22 +0700

    [2885] [Time Beats: 51.62%] - LeetPush

--- Pandas/2885-Rename-Columns.py
+++ Pandas/2885-Rename-Columns.py
@@ -1,9 +1,18 @@
 import pandas as pd
 
 def renameColumns(students: pd.DataFrame) -> pd.DataFrame:
-    return students.rename(columns={
+    students.rename(columns={
         'id':'student_id',
         'first':'first_name',
         'last':'last_name',
         'age':'age_in_years'
-        })
+    }, inplace=True) # Ensures that the DataFrame is modified in place/changes are applied directly to the original DataFrame.
+    return students # Need to include if you put inplace=True
+
+""" return students.rename(columns={
+    'id': 'student_id',
+    'first': 'first_name',
+    'last': 'last_name',
+    'age': 'age_in_years'
+    })
+"""
